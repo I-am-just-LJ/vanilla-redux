28e30156cd5f5d796a747ff36017a801900a1aa6
LJ <jhlee9193@gmail.com>
2020-10-29 09:32:26 +0900

    store and reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  return state;
 };
 
-const handlePlus = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
